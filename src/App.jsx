05a8aad72a250b76080a86d60f5a93c30ffc53c4
Muhammad-ul-bakir <bakirmuhammad74@gmail.com>
2023-08-13 17:20:24 +0500

    fourth

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
-import React from 'react'
-import Helmet from "react-helmet";
+import React from "react";
 
-import Pagebound from './PageRoutes/Pagebound';
-import Dashboard from './MainPage/Dashboard';
+import Pagebound from "./PageRoutes/Pagebound";
+import Dashboard from "./MainPage/Dashboard";
 const App = () => {
   return (
     <>
-      <Helmet>
-  <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
-  <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="../assets/js/sidebarmenu.js"></script>
-  <script src="../assets/js/app.min.js"></script>
-  <script src="../assets/libs/apexcharts/dist/apexcharts.min.js"></script>
-  <script src="../assets/libs/simplebar/dist/simplebar.js"></script>
-  <script src="../assets/js/dashboard.js"></script>
-      </Helmet>
-    {/*  Body Wrapper */}
-  <Pagebound/>
+      {/*  Body Wrapper */}
+      <Pagebound />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Helmet from "react-helmet";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Helmet>
+      <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
+      <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+      <script src="../assets/js/sidebarmenu.js"></script>
+      <script src="../assets/js/app.min.js"></script>
+      <script src="../assets/libs/apexcharts/dist/apexcharts.min.js"></script>
+      <script src="../assets/libs/simplebar/dist/simplebar.js"></script>
+      <script src="../assets/js/dashboard.js"></script>
+    </Helmet>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
